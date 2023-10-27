f7ae05110f5f7b4e7f8c2560cfff26e40ee5995b
Guru <ping.aipl@gmail.com>
2023-10-27 20:52:35 +0530

    completed

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+
 import os
 from pyexpat import model
 from statistics import mode
+from tabnanny import verbose
+from turtle import onclick
 from dotenv import load_dotenv
 import json
 import requests
@@ -10,6 +13,20 @@ from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
+from langchain.tools import BaseTool
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.schema import SystemMessage
+from langchain.prompts import MessagesPlaceholder
+from langchain.memory import ConversationSummaryBufferMemory
+
+from pydantic import BaseModel, Field
+from typing import Type
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from uvicorn import run
 
 load_dotenv()
 
@@ -28,7 +45,7 @@ def search(query):
         'Content-Type': 'application/json'
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+    # print(response.text)
     return response.text
 
 
@@ -79,14 +96,10 @@ def summarize(objective:str, content:str):
     docs = text_splitter.create_documents([content])
     
     map_prompt = """
-        You are an expeienced research agent who can perform research of given context and produce a detailed report. 
-        The research has an objective mentioned below. 
-        The text given below is your context.  
-        Your report should provide introduction, key details, major achievements, significant events etc. 
-        The report can include contact details such as office location, address, email IDs or telephone numbers.
-        Use list of items aproach when listing items. 
+        Provide a precise summary of given context for the given objective.
         Objective : {objective}
         Text : {text}
+        Summary : 
     """
     map_prompt_template = PromptTemplate(
         input_variables=["text", "objective"],
@@ -103,9 +116,80 @@ def summarize(objective:str, content:str):
     output = summary_chain.run(input_documents = docs, objective=objective)
     return output
 
-if __name__ == "__main__":
-    # query = input('Enter a search query: ')
-    # search(query)
-    test_url = 'https://www.youtube.com/watch?v=5HBGEPbIQGo'
-    resp = scrape_website('short essay', test_url)
-    print(resp)
+# To use the function scrape_website as tool, we need to define a toolwrapper for this function
+
+class ScrapeWebsiteInput(BaseModel):
+    objective:str = Field(description="The objective and task that user give to agent")
+    url:str = Field(description="the url of the website to be scraped")
+
+class ScrapeWebSiteTool(BaseTool):
+    name= "scrape_website"
+    description= "useful when you want to scrape data from website url by passing both url and objective to function"
+    args_schema: Type[BaseModel] = ScrapeWebsiteInput
+
+    def _run(self, objective:str, url:str):
+        return scrape_website(objective, url)
+
+    def _arun(self, url:str):
+        raise NotImplementedError("Not implemeted")
+
+
+# Create langchain agent with tools defined above
+tools = [
+    Tool(
+        name = "search",
+        func = search,
+        description="ueseful when information like current event, data needed from internet. Provide precise query for search"
+    ),
+    ScrapeWebSiteTool()
+]
+
+system_message = SystemMessage(
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
+            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            
+            Please make sure you complete the objective above with the following rules:
+            1/ You should do enough research to gather as much information as possible about the objective
+            2/ If there are url of relevant links & articles, you will scrape it to gather more information
+            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iterations
+            4/ You should not make things up, you should only write facts & data that you have gathered
+            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+)
+
+agent_kwargs = {
+    "extra_prompt_messages" : [MessagesPlaceholder(variable_name="memory")],
+    "system_message": system_message 
+}
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
+memory = ConversationSummaryBufferMemory(
+    memory_key="memory",
+    return_messages=True,
+    llm=llm,
+    max_token_limit=1000
+)
+
+agent = initialize_agent(
+    tools=tools,
+    llm=llm,
+    agent=AgentType.OPENAI_FUNCTIONS,
+    verbose=True,
+    agent_kwargs=agent_kwargs,
+    memory=memory
+)
+
+app = FastAPI()
+class Query(BaseModel):
+    query: str
+
+@app.post('/')
+async def research(query: Query):
+    content = agent({"input": query})
+    return content
+
+
+
+# if __name__ == "__main__":
+#     run(app)
+#     # search(query)
